31f0365c7ee756dee28c3a963121ae2037cdab0c
Chris Pressey <cpressey@catseye.tc>
2019-03-20 16:42:30 +0000

    Escape HTML characters in output.

--- src/HasteMain.hs
+++ src/HasteMain.hs
@@ -8,9 +8,15 @@ import Qlzqqlzuup
 
 main = withElems ["prog", "result", "run-button"] driver
 
+escapeHTML "" = ""
+escapeHTML ('<' : rest) = "&lt;" ++ escapeHTML rest
+escapeHTML ('>' : rest) = "&gt;" ++ escapeHTML rest
+escapeHTML ('&' : rest) = "&amp;" ++ escapeHTML rest
+escapeHTML (c   : rest) = (c : escapeHTML rest)
+
 driver [progElem, resultElem, runButtonElem] = do
     onEvent runButtonElem Click $ \_ -> execute
     where
         execute = do
             Just prog <- getValue progElem
-            setProp resultElem "innerHTML" (showRun prog)
+            setProp resultElem "innerHTML" (escapeHTML $ showRun prog)
